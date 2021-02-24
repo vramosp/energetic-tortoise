cb10315023c651dffea10620361dc8c096e3520f
Nicolas Grenie <nicolas.grenie@gmail.com>
2021-02-24 11:22:32 -0800

    embed typeform

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,30 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === 'build-html') {
+        actions.setWebpackConfig({ 
+            module: {
+                rules: [
+                  {
+                    test: /@typeform\/embed/,
+                    use: loaders.null(),
+                  },
+                ],
+              }
+        })
+    }
+}
+
+exports.onCreateBabelConfig = ({ actions }) => {
+    if (process.env.NODE_ENV !== 'development') {
+      actions.setBabelPlugin({
+        name: '@babel/plugin-transform-regenerator',
+        options: {},
+      });
+      actions.setBabelPlugin({
+        name: '@babel/plugin-transform-runtime',
+        options: {},
+      });
+    }
+  };
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
-import {graphql} from 'gatsby';
+import { graphql } from 'gatsby';
 
-import {Layout} from '../components/index';
-import {withPrefix, htmlToReact} from '../utils';
+import { Layout } from '../components/index';
+import { withPrefix, htmlToReact } from '../utils';
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
@@ -16,28 +16,45 @@ export const query = graphql`
   }
 `;
 
-export default class Post extends React.Component {
-    render() {
-        return (
-            <Layout {...this.props}>
+export default function Post(props) {
+
+    // const [typeform, setTypeform] = useState(null);
+    const typeformRef = useRef(null)
+
+
+    const typeform = useRef(null);
+    console.log('props',props)
+
+    useEffect(() => {
+        (async () => {
+            const { makeWidget } = await import("@typeform/embed");
+            if (typeform.current) {
+                makeWidget(typeform.current, `https://picsoung.typeform.com/to/SL09LYJ3#product_name=${props.pageContext}&product_id=xxxxx`, {});
+            }
+        })();
+    }, []);
+    return (
+        <Layout {...props}>
             <section className="post">
-                {_.get(this.props, 'pageContext.frontmatter.content_img_path', null) && (
-                <img className="header-image" src={withPrefix(_.get(this.props, 'pageContext.frontmatter.content_img_path', null))} alt={_.get(this.props, 'pageContext.frontmatter.content_img_alt', null)}/>
+                {_.get(props, 'pageContext.frontmatter.content_img_path', null) && (
+                    <img className="header-image" src={withPrefix(_.get(props, 'pageContext.frontmatter.content_img_path', null))} alt={_.get(props, 'pageContext.frontmatter.content_img_alt', null)} />
                 )}
                 <header className="hero">
                     <div className="copy">
-                        <h1>{_.get(this.props, 'pageContext.frontmatter.title', null)}</h1>
-                        {_.get(this.props, 'pageContext.frontmatter.subtitle', null) && (
-                        <h3>{htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle', null))}</h3>
+                        <h1>{_.get(props, 'pageContext.frontmatter.title', null)}</h1>
+                        {_.get(props, 'pageContext.frontmatter.subtitle', null) && (
+                            <h3>{htmlToReact(_.get(props, 'pageContext.frontmatter.subtitle', null))}</h3>
                         )}
-                        <h3 className="publish-date">{moment(_.get(this.props, 'pageContext.frontmatter.date', null)).strftime('%A, %B %e, %Y')}</h3>
+                        <h3 className="publish-date">{moment(_.get(props, 'pageContext.frontmatter.date', null)).strftime('%A, %B %e, %Y')}</h3>
                     </div>
                 </header>
                 <div className="content">
-                    {htmlToReact(_.get(this.props, 'pageContext.html', null))}
+                    {htmlToReact(_.get(props, 'pageContext.html', null))}
                 </div>
+                <div ref={typeform} style={{ height: '100vh', width: '100%' }}></div>
+
             </section>
-            </Layout>
-        );
-    }
+        </Layout>
+    );
+    // }
 }
