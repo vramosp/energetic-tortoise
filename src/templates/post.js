fd2d44513d8eaebf22b26d32355e9aa37e9f4c83
Nicolas Grenie <nicolas.grenie@gmail.com>
2021-02-25 13:41:10 -0800

    add typeform link

--- src/templates/post.js
+++ src/templates/post.js
@@ -58,6 +58,11 @@ export default function Post(props) {
                         </div>
                     ))}
                 </div>
+
+                <div>
+                    <h2>Leave a review</h2>
+                    <a href={`https://picsoung.typeform.com/to/SL09LYJ3#product_name=${props.pageContext.frontmatter.title}&product_id=xxxxx`}> Review</a>
+                </div>
             </section>
         </Layout>
     );
