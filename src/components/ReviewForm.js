9e978014b68dbcf1d5bbce576a03ce70d7ee77e3
Stackbit Code Editor <support@stackbit.com>
2021-03-09 21:07:21 +0000

    ReviewForm.js: changed

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -5,7 +5,7 @@ const ReviewForm = (product_name) =>{
   const typeformRef = useRef(null)
 
   useEffect(() => {
-    typeformEmbed.makeWidget(typeformRef.current, 'https://form.typeform.com/to/SL09LYJ3#product_name=', {
+    typeformEmbed.makeWidget(typeformRef.current, `https://form.typeform.com/to/SL09LYJ3#product_name=`, {
       hideFooter: true,
       hideHeaders: true,
       opacity: 50,
