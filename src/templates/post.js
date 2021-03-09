1bc326db9de28d82c92a7ba47de0009d89d64031
Stackbit Code Editor <support@stackbit.com>
2021-03-09 20:57:07 +0000

    post.js: changed

--- src/templates/post.js
+++ src/templates/post.js
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 
 import { Layout } from '../components/index';
 import { withPrefix, htmlToReact } from '../utils';
+import * as typeformEmbed from '@typeform/embed'
 
 // this minimal GraphQL query ensures that when 'gatsby develop' is running,
 // any changes to content files are reflected in browser
