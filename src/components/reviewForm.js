9dd67b132885d1771c68a2881431afacc4c4a001
Stackbit Code Editor <support@stackbit.com>
2021-03-09 20:59:15 +0000

    reviewForm.js: changed

---
+++ src/components/reviewForm.js
@@ -0,0 +1,16 @@
+import React, { useRef, useEffect } from 'react'
+import * as typeformEmbed from '@typeform/embed'
+
+const MyTypeform = () => {
+  const typeformRef = useRef(null)
+
+  useEffect(() => {
+    typeformEmbed.makeWidget(typeformRef.current, 'https://form.typeform.com/to/MY_TYPEFORM_ID', {
+      hideFooter: true,
+      hideHeaders: true,
+      opacity: 50,
+    })
+  }, [typeformRef])
+
+  return <div ref={typeformRef} style={{ height: '100vh', width: '100%' }}></div>
+}
