fe5be4eea1a00a242397b6a3f7fda5b62011edc6
Stackbit Code Editor <support@stackbit.com>
2021-03-09 21:05:02 +0000

    ReviewForm.js: changed

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react'
 import * as typeformEmbed from '@typeform/embed'
 
-export default class ReviewForm extends React.Component {
+export default  ReviewForm extends React.Component {
   const typeformRef = useRef(null)
 
   useEffect(() => {
